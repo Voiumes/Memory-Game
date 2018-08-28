1ba9cd849828e68154e982cf2df882dbd6457ea7
Sean <omerta201@yahoo.com>
2018-08-28 14:25:41 -0400

    changed from grid to flexbox instead

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const cards = document.querySelectorAll('.card')
+let hasFlippedCard = false
+let firstCard;
+let secondCard;
+
+
 
 function flipCard(){
-    console.log('iwasclicked');
     this.classList.toggle('flip');
-    this.classList.toggle('face');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+    }else{
+        hasFlippedCard = false;
+        secondCard = this;
+        console.log(firstCard.dataset.cardtype)
+        console.log(secondCard.dataset.cardtype)
 }
+};
 
-cards.forEach(card => card.addEventListener('click', flipCard))
+cards.forEach(card => card.addEventListener('click', flipCard));
